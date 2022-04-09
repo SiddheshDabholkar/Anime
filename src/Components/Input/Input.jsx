43e0da7b43884a4211e44ffbbad4d3c03fc13e02
Siddhesh Dabholkar <iamsiddhesh22@gmail.com>
2022-04-09 15:19:02 +0530

    show fetched anime on first render

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -2,5 +2,12 @@ import React from "react";
 import "./Input.scss";
 
 export default function Input({ onChange, value }) {
-  return <input className="Input" onChange={onChange} value={value} />;
+  return (
+    <input
+      className="Input"
+      onChange={onChange}
+      value={value}
+      placeholder="Search Anime name"
+    />
+  );
 }
